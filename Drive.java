2a8eeb2b7dfdc4f620f8b7c7e080447513f9790a
ThatDucky <loganpetty3232@gmail.com>
2020-10-28 13:49:45 -0600

    made drive drive

--- Drive.java
+++ Drive.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import java.lang.Math;
@@ -6,37 +7,42 @@ import java.lang.Math;
 @TeleOp(name = "Drive", group = "Drive")
 
 public class Drive extends OpMode {
-        Hardware robot = new Hardware();
+    Hardware robot = new Hardware();
 
-        @Override
-        public void init() {
-                robot.init(hardwareMap);
-                robot.setMode(2); //set to run without encoders
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+        robot.setMode(2); //set to run without encoders
+        telemetry.addData("Status", "Ready");
+        telemetry.update(); //setup telemetry and call it
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+        telemetry.addData("Status", "Running");
+        telemetry.update(); //setup telemetry and call it
+    }
+
+    @Override
+    public void loop() {
+        double deadZone = 0.12;
+        telemetry.addData("Left Stick Position", gamepad1.left_stick_x + " " + gamepad1.left_stick_y);
+        telemetry.update();
+
+        if(gamepad1.left_stick_y > deadZone || gamepad1.left_stick_y < (deadZone * -1)){
+                //set both sides to positive/negative power for going forward or backward
+                robot.setPower(gamepad1.left_stick_y, gamepad1.left_stick_y);
+        }else if(gamepad1.left_stick_x > deadZone || gamepad1.left_stick_x < (deadZone * -1)){
+                 //left side gets negative power for turning
+                robot.setPower((gamepad1.left_stick_x * -1), gamepad1.left_stick_x);
+        }else{
+                //if no joystick input reset to 0 power
+                robot.setPower(0,0);
         }
-
-        @Override
-        public void init_loop() {
-
-        }
-
-        @Override
-        public void start() {
-
-        }
-
-        @Override
-        public void loop() {
-                double deadZone = 0.12;
-
-                if(gamepad1.left_stick_y > deadZone || gamepad1.left_stick_y < (deadZone * -1)){
-                        //set both sides to positive/negative power for going forward or backward
-                        robot.setPower(gamepad1.left_stick_y, gamepad1.left_stick_y);
-                }else if(gamepad1.left_stick_x > deadZone || gamepad1.left_stick_x < (deadZone * -1)){
-                        //left side gets negative power for turning
-                        robot.setPower((gamepad1.left_stick_x * -1), gamepad1.left_stick_x);
-                }else{
-                        //if no joystick input reset to 0 power
-                        robot.setPower(0,0);
-                }
-        }
+    }
 }
